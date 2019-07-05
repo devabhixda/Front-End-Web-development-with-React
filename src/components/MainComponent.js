9586b8eeed127677990d2ec04d386d59edb5a8b3
Abhi Jain <devabhixda@gmail.com>
2019-07-05 17:14:19 +0530

    React Router
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -21,6 +23,14 @@ class Main extends Component {
   }
 
   render() {
+
+
+    const HomePage = () => {
+      return(
+          <Home
+          />
+      );
+    }
     return (
       <div>
         <Navbar dark color="primary">
@@ -29,7 +39,11 @@ class Main extends Component {
           </div>
         </Navbar>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
       </div>
